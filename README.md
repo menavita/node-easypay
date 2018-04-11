af5d2152e70c678ff06d4413015c04fbdf10029c
Yaroslau Bukatkin <yaroslaubuk@gmail.com>
2018-04-11 16:56:02 +0300

    Added checkSignature and createSignature functions. Function createInvoice removed as useless.

--- README.md
+++ README.md
@@ -3,16 +3,13 @@ Node module for [Easypay API.](https://ssl.easypay.by/light/)
 
 ```javascript
 var Easypay = require('node-easypay');
-
-var easypay = new Easypay({ EP_MerNo: 'ok6666', web_key: '11111111' });
-
 ```
 ## Functions
 Currently SOAP is not implemented.
 
-### Create invoice
+### Create signature. Return signature as string.
 ```javascript
-easypay.createInvoice({
+var signature = Easypay.createSignature({
   'EP_OrderNo': '12312412',
   'EP_Sum': 12.50,
   'EP_Expires': 2,
@@ -20,13 +17,13 @@ easypay.createInvoice({
   'EP_OrderInfo': 'Тест',
   'EP_Success_URL': 'http://mytestshop.by/success/',
   'EP_Cancel_Url': 'http://mytestshop.by/cancel/',
-  'EP_Debug': '0',
-  'EP_Encoding': 'utf-8'
-}).then(function(res) {
-  console.log(res);
-}).catch(function(err) { 
-  throw err;
-})
+  'EP_Debug': '0'
+}, 'secret_key');
+```
+
+### Check signature. Return boolean value.
+```javascript
+Easypay.checkSignature(params_from_easypay, 'secret_key');
 ```
 
 ## Tests
--- test/createInvoice.test.js
+++ test/createInvoice.test.js
@@ -1,12 +1,10 @@
 var should = require('should');
 var Easypay = require('../');
 
-var easypay = new Easypay({ EP_MerNo: 'ok6666', web_key: '11111111' }, true);
-
 describe('Easypay', function() {
 
-  it('create invoice', function() {
-    return easypay.createInvoice({
+  it('should create signature', function() {
+    var signature = Easypay.createSignature({
       'EP_OrderNo': '12312412',
       'EP_Sum': 12.50,
       'EP_Expires': 2,
@@ -14,13 +12,26 @@ describe('Easypay', function() {
       'EP_OrderInfo': 'Тест',
       'EP_Success_URL': 'http://mytestshop.by/success/',
       'EP_Cancel_Url': 'http://mytestshop.by/cancel/',
-      'EP_Debug': '0',
-      'EP_Encoding': 'utf-8'
-    }).then(function(res) {
-      console.log(res);
-    }).catch(function(err) { 
-      throw err;
-    })
+      'EP_Debug': '0'
+    }, 'secret_key');
+    console.log(signature);
+  })
+
+  it("should check signature. Don't have response examle right now.", function() {
+    var check = Easypay.checkSignature({
+      order_mer_code,
+      sum,
+      mer_no,
+      card,
+      purch_date,
+      notify_signature,
+      xml_data 
+    }, 'secret_key');
+    if (check) {
+      console.log('True signature');
+    } else {
+      throw new Error('False signature');
+    }
   })
 
 })
